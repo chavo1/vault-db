db61520fa62a8b7c97652faadbfda97c732a0974
chavo1 <42143239+chavo1@users.noreply.github.com>
2018-11-19 18:02:29 +0200

    Update bind-add.sh

--- scripts/bind-add.sh
+++ scripts/bind-add.sh
@@ -4,7 +4,7 @@
 echo "Updating mysql configs in /etc/mysql/mysql.conf.d/mysqld.cnf."
 sudo sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
 echo "Updated mysql bind address in /etc/mysql/mysql.conf.d/mysqld.cnf to 0.0.0.0 to allow external connections."
-echo "Reastarting mysql service..."
+echo "Restarting mysql service..."
 sudo /etc/init.d/mysql stop
 sudo /etc/init.d/mysql start
 echo "Done"
